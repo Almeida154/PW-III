bac369c0a02d7edc755484e5f906282f53c08fe0
Almeida154 <davidalmeida154of@gmail.com>
2021-09-06 18:01:25 -0300

    :tada: Viacep with php initial
    a project that fill the street field automatically just with cep field

---
+++ ViaCepPHP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '729429c050b3046376f12a13c56b844b5738c729',
+        'name' => 'viacep/php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'viacep/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '729429c050b3046376f12a13c56b844b5738c729',
+            'dev_requirement' => false,
+        ),
+    ),
+);
